7f0ef8e6ef1fe60de18be12f83613cc4050f7d22
anandajuliar <anandajulia001@gmail.com>
2024-03-07 19:29:46 +0700

    membuat prosedur display

--- BubbleSort/BubbleSort.cpp
+++ BubbleSort/BubbleSort.cpp
@@ -49,6 +49,24 @@ void BubbleSortArray() {
     } while (pass <= n - 1);  
 }
 
+void  display() {
+    cout << endl;
+    cout << "=================================" << endl;
+    cout << "Element Array yang telah tersusun" << endl;
+    cout << "=================================" << endl;
+    cout << endl;
+    for (int j = 0; j < n; j++) {
+        cout << a[j];
+        if (j < n - 1) {
+            cout << " --->";
+        }
+    }
+    cout << endl;
+    cout << endl;
+    cout << "Jumlah pass =" << n - 1 << endl;
+    cout << endl;
+}
+
 
 int main() {
 
